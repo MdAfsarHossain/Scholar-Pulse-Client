a2649ba2c8b68530a790e9384cc827440a420e49
MdAfsarHossain <afsar291722@gmail.com>
2025-01-22 19:26:25 +0600

    User Route jsx file added

---
+++ src/routes/UserRoute.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types'
+import { Navigate } from 'react-router-dom'
+import LoadingSpinner from '../components/Shared/LoadingSpinner'
+import useRole from '../hooks/useRole'
+
+const UserRoute = ({ children }) => {
+    const [role, isLoading] = useRole()
+
+    if (isLoading) return <LoadingSpinner />
+    if (role === 'User') return children
+    return <Navigate to='/dashboard' replace='true' />
+}
+
+UserRoute.propTypes = {
+    children: PropTypes.element,
+}
+
+export default UserRoute
